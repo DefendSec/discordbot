6a566d6d97035ad0efd6e4460e3734d3c9139a9d
Dave <daveistdave@gmail.com>
2023-04-07 23:46:52 +0200

    comments

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import re
 # All channel ids that the bot should look at here
 # Seperate with comma
 channel_id = [1087310970700967947]
+# replace this
 #
-#
+# When it shows my token, then you are fired
 token = "MTA4ODk0MDYyODk3ODcxMjY4Ng.GLJld0.S9qsXMCeJXVjESTs-JrW2MB2Dh9y1iv3n3g7c0"
 
 
